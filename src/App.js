7770b8d6bbb072721ab4e1114b3ce75dcbce4510
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2022-12-07 12:44:22 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer className="App-footer">
-        This project was coded by{" "}
-        <a href="https://www.emmelineocampo.me/">Emmeline Ocampo</a> and it is{" "}
-        <a href="https://github.com/EmmeRox/weather-react-final">
-          open-sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a href="https://www.emmelineocampo.me/">Emmeline Ocampo</a> and it is{" "}
+          <a href="https://github.com/EmmeRox/weather-react-final">
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
