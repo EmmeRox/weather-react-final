d592b872fa6acd73901da76071dc66fc514fae9f
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2022-12-07 12:25:16 -0600

    Added git repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <h1>Weather App</h1>
+      <footer className="App-footer">
+        This project was coded by{" "}
+        <a href="https://www.emmelineocampo.me/">Emmeline Ocampo</a> and it is{" "}
+        <a href="https://github.com/EmmeRox/weather-react-final">
+          open-sourced.
+        </a>
+      </footer>
     </div>
   );
 }
