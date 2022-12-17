4eda8f2c67c8ec19ebd70f9147eb6ce477dda348
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2022-12-16 18:19:29 -0600

    Added states and organizing code

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css";
-import axios, { Axios } from "axios";
+import axios from "axios";
 
 export default function Forecast(props) {
+  let [forecast, setForecast] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let key = "1a24eda60f6a073a6acaft9odbaee084";
-  let city = props.data;
-  let units = "metric";
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}&units=${units}`;
+  console.log(forecast[0]);
 
-  axios.get(apiURL).then(handleResponse);
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col">
-          <div className="Forecast-day">Thu</div>
-          <img src={props.data.icon} alt={props.data.condition} />
-          <div className="Forecast-temp">
-            <span className="Forecast-max">20°</span>
-            <span className="Forecast-min">10°</span>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col">
+            <div className="Forecast-day">Thu</div>
+            <img src={forecast[0].condition.icon} alt={forecast[0].condition} />
+            <div className="Forecast-temp">
+              <span className="Forecast-max">20°</span>
+              <span className="Forecast-min">10°</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let key = "1a24eda60f6a073a6acaft9odbaee084";
+    let city = props.data;
+    let units = "metric";
+    let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${key}&units=${units}`;
+
+    axios.get(apiURL).then(handleResponse);
+    return "Loading";
+  }
 }
